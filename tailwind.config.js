935048cf2b6f250f172ba1e180eb51f16ec22f33
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-03-20 20:03:02 +0800

    Added: Custom Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      "c-teal-light": "#99D5C9",
-      "c-teal-dark": "#6C969D",
-      "c-purple-light": "#645E9D",
-      "c-purple-dark": "#392B58",
-      "c-purple-darker": "#2D0320",
+    extend: {
+      colors: {
+        "c-teal-light": "#99D5C9",
+        "c-teal-dark": "#6C969D",
+        "c-purple-light": "#645E9D",
+        "c-purple-dark": "#392B58",
+        "c-purple-darker": "#2D0320",
+      },
     },
   },
   variants: {
