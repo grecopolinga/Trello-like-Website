12a5d2f15653edd1f4fdc41b499d911601fc44ad
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-03-31 23:55:50 +0800

    Modified Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "c-teal-light": "#99D5C9",
+        "c-teal-light": "#80FFFF",
         "c-teal-dark": "#6C969D",
         "c-purple-light": "#645E9D",
-        "c-purple-dark": "#392B58",
-        "c-purple-darker": "#2D0320",
+        "c-purple-dark": "#B19CD9",
+        "c-purple-darker": "#6E65C4",
+        "c-whitesmoke": "#F5F5F5",
+        webcolor: {
+          "light-1": "#F8F7F7",
+          "light-2": "#84ADC5",
+          "base-color": "#63BBE2",
+          "dark-1": "#7DA5BE",
+          "dark-2": "#1F81C4",
+        },
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 };
