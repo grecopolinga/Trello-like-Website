d0f34ac2e7ae28346fa129cf339699f536f43cb0
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-04-02 22:04:06 +0800

    Added: Script for home page

---
+++ web/js/home.js
@@ -0,0 +1,20 @@
+const boardSettingsModal = document.getElementById("board-add-modal");
+const boardSettingsShow = document.getElementById("board-add-button");
+const boardSettingsConfirm = document.getElementById("board-add-confirm");
+
+const closeModal = document.querySelectorAll(".close-modal");
+
+boardSettingsShow.addEventListener("click", () => {
+  boardSettingsModal.classList.remove("hidden");
+});
+
+boardSettingsConfirm.addEventListener("click", () => {
+  boardSettingsModal.classList.add("hidden");
+  return window.location.replace("workspace.html");
+});
+
+closeModal.forEach((close) => {
+  close.addEventListener("click", () => {
+    boardSettingsModal.classList.add("hidden");
+  });
+});
