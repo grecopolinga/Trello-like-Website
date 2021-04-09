99ed38858ec51b9ad306a2d91e4cc60b4c76187e
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-04-09 23:15:48 +0800

    Added: console log at app.listen

--- app.js
+++ app.js
@@ -27,4 +27,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(`Serving at http://localhost:${PORT}`);
 });
