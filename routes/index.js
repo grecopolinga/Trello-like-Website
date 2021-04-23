44f34d37f60e72a5709a82bafd228dc84ba30de3
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-04-23 18:32:49 +0800

    Added: Ctrls

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const uuid = require('uuid');
+const signupCtrl = require('../controller/signupCtrl');
+const signinCtrl = require('../controller/signinCtrl');
+const ctrl = require('../controller/controller');
 
-const users = [];
-// Landing page
-router.get('/', (req, res) => {
-    res.render('index');
-});
+// @desc Landing page
+// @route GET /
+router.get('/', ctrl.getIndex);
 
-// Login page
-router.get('/login', (req, res) => {
-    res.render('login', { layout: 'login', error: false });
-});
+// @desc Login page
+// @route GET /login
+router.get('/login', ctrl.getLogin);
 
-// Login
-router.post('/login', async (req, res) => {
-    const user = users.find((user) => user.username === req.body.username);
-    console.log(user);
-    if (user == null) {
-        return res.render('login', {
-            layout: 'login',
-            error: true,
-            msg: 'Incorrect Username/Password',
-        });
-    }
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.send('Success');
-        } else {
-            res.render('login', {
-                layout: 'login',
-                error: true,
-                msg: 'Incorrect Username/Password',
-            });
-        }
-    } catch (err) {
-        res.status(500).send();
-    }
-});
+// @desc Login user
+// @route POST /login
+router.post('/login', signinCtrl.loginUser);
 
-// Register
-router.post('/register', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.reg_password, 10);
-        users.push({
-            id: uuid.v4(),
-            name: req.body.firstname + ' ' + req.body.lastname,
-            username: req.body.reg_username,
-            email: req.body.email,
-            password: hashedPassword,
-        });
-        res.send(users);
-    } catch (err) {
-        res.redirect('/login');
-    }
-});
+// @desc Register User
+// @route POST /register
+router.post('/register', signupCtrl.registerUser);
 
 module.exports = router;
