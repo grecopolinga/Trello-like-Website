ac77959287c6ce7fcaeea73e34d850a27a1c20cb
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-04-25 01:30:09 +0800

    Added: userBoards, userFavorites, userSettings

--- controller/controller.js
+++ controller/controller.js
@@ -1,3 +1,5 @@
+const Users = require('../models/Users');
+
 const ctrl = {
     getIndex: (req, res) => {
         res.render('index');
@@ -5,6 +7,34 @@ const ctrl = {
 
     getLogin: (req, res) => {
         res.render('login', { layout: 'login', error: false });
+    },
+
+    userBoards: (req, res) => {
+        const user = req.params.username;
+        res.render('myboards', { layout: 'home', user: user });
+    },
+
+    userFavorites: (req, res) => {
+        const user = req.params.username;
+        res.render('favorites', { layout: 'home', user: user });
+    },
+
+    userSettings: async (req, res) => {
+        try {
+            const user = await Users.findOne({ username: req.params.username });
+            const details = {
+                fName: user.fName,
+                lName: user.lName,
+                email: user.email,
+            };
+            const options = { layout: 'home', user: user.username };
+            res.render('settings', {
+                ...options,
+                ...details,
+            });
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
     },
 };
 
