23446eb246104def8eda52f2c688f234b4a14e01
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-04-25 01:31:21 +0800

    Added: user routes

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,40 @@ const express = require('express');
 const router = express.Router();
 const signupCtrl = require('../controller/signupCtrl');
 const signinCtrl = require('../controller/signinCtrl');
+// const boardCtrl = require('../controller/boardCtrl');
+const userCtrl = require('../controller/userCtrl');
 const ctrl = require('../controller/controller');
-
-// @desc Landing page
+const findUserBoard = require('../middleware/findUserBoard');
+// @desc  Landing page
 // @route GET /
 router.get('/', ctrl.getIndex);
 
-// @desc Login page
+// @desc  Login page
 // @route GET /login
 router.get('/login', ctrl.getLogin);
 
-// @desc Login user
+// @desc  Login user
 // @route POST /login
 router.post('/login', signinCtrl.loginUser);
 
-// @desc Register User
+// @desc  Register User
 // @route POST /register
 router.post('/register', signupCtrl.registerUser);
+
+// @desc  User Boards
+// @route GET /:username/favorites
+router.get('/:username/boards', findUserBoard, ctrl.userBoards);
+
+// @desc  User Favorites
+// @route GET /:username/favorites
+router.get('/:username/favorites', findUserBoard, ctrl.userFavorites);
+
+// @desc User Settings
+// @route GET /:username/settings
+router.get('/:username/settings', findUserBoard, ctrl.userSettings);
+
+router.post('/:username/settings', userCtrl.updateUser);
+
+router.delete('/:username/settings', userCtrl.deleteUser);
 
 module.exports = router;
