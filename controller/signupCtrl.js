fe90710464312e539a4e273c1d8a5e7dd15fa0f7
HMoneda <80668451+HMoneda@users.noreply.github.com>
2021-04-27 01:30:55 +0800

    Added: Validation Procedure

--- controller/signupCtrl.js
+++ controller/signupCtrl.js
@@ -1,22 +1,52 @@
 const Users = require('../models/Users');
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
 const signupCtrl = {
     registerUser: async (req, res) => {
         try {
-            const hashedPassword = await bcrypt.hash(req.body.reg_password, 10);
-            const user = new Users({
-                fName: req.body.firstname,
-                lName: req.body.lastname,
-                username: req.body.reg_username,
-                email: req.body.email,
-                password: hashedPassword,
-                img: 'default.png',
-            });
-            const newUser = await user.save();
-            res.redirect(`/${newUser.username}/boards`);
+            var errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                errors = errors.errors;
+
+                var details = {};
+                for (let i = 0; i < errors.length; i++)
+                    details[errors[i].param + 'Error'] = errors[i].msg;
+
+                res.render('login', details);
+            } else {
+                const hashedPassword = await bcrypt.hash(
+                    req.body.reg_password,
+                    10
+                );
+                const user = new Users({
+                    fName: req.body.firstname,
+                    lName: req.body.lastname,
+                    username: req.body.reg_username,
+                    email: req.body.email,
+                    password: hashedPassword,
+                    img: 'default.png',
+                });
+                const newUser = await user.save();
+                res.redirect(`/${newUser.username}/boards`);
+            }
         } catch (err) {
             res.redirect('/login');
+        }
+    },
+
+    getCheckUser: async (req, res) => {
+        try {
+            console.log(req.query.username);
+            const user = await Users.findOne({ username: req.query.username });
+            if (user == null) {
+                res.send(true);
+            } else {
+                res.send(false);
+            }
+        } catch (err) {
+            res.status(500).send(err);
         }
     },
 };
