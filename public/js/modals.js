78b827f7d5bcf11ebff8aa7ef24f1500f8add6b9
JMarie39 <juliana.marie39@gmail.com>
2021-05-08 23:37:11 +0800

    Added: validator in settings

--- public/js/modals.js
+++ public/js/modals.js
@@ -1,4 +1,7 @@
 const closeModal = document.querySelectorAll('.close-modal');
+let isValidPassword = true;
+let isValidEmail = true;
+let isUpdate = false;
 
 $('#deactivate-button').click(() => {
     $('#deactivate-modal').removeClass('hidden');
@@ -8,8 +11,55 @@ $('#deactivate-confirm').click(() => {
     $('#deactivate-modal').addClass('hidden');
 });
 
+$('#password').keyup(() => {
+    let password = $('#password').val();
+
+    if (password.length < 8 && password.length > 0) {
+        $('#passwordError').removeClass('hidden');
+        $('#updateError').addClass('hidden');
+        isValidPassword = false;
+    } else if (password.length == 0) {
+        $('#passwordError').addClass('hidden');
+        $('#updateError').addClass('hidden');
+        isValidPassword = true;
+    } else {
+        $('#passwordError').addClass('hidden');
+        $('#updateError').addClass('hidden');
+        isValidPassword = true;
+    }
+});
+
+$('#email').keyup(() => {
+    let email = $('#email').val();
+
+    console.log(email.includes('@'));
+    if (!email.includes('@') && email.length > 0) {
+        $('#emailError').removeClass('hidden');
+        $('#updateError').addClass('hidden');
+        isValidEmail = false;
+    } else if (email.length == 0) {
+        $('#emailError').addClass('hidden');
+        $('#updateError').addClass('hidden');
+        isValidEmail = true;
+    } else {
+        $('#emailError').addClass('hidden');
+        $('#updateError').addClass('hidden');
+        isValidEmail = true;
+    }
+});
+
 $('#save-button').click(() => {
-    $('#save-modal').removeClass('hidden');
+    if ($('#password').val().length == 0 && $('#email').val().length == 0) {
+        isUpdate = false;
+        $('#updateError').removeClass('hidden');
+    } else {
+        isUpdate = true;
+        $('#updateError').addClass('hidden');
+    }
+
+    if (isValidPassword && isValidEmail && isUpdate) {
+        $('#save-modal').removeClass('hidden');
+    }
 });
 
 $('#save-confirm').click((e) => {
